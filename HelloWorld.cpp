d4bac72b723bcafe396ff666c5fab18804be8817
Jeremie Gaudet <202269051@cegepsherbrooke.qc.ca>
2023-08-25 11:22:00 -0400

    fichier-fonctinos2

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include "fonctions.h"
 
 using namespace std;
 
 void main() {
-	cout << "Hello World!" << endl;
+	helloWorld();
 }
---
+++ fonctions.cpp
@@ -0,0 +1,7 @@
+#include "fonctions.h"
+#include <iostream>
+using namespace std;
+
+void helloWorld() {
+	cout << "coucou tout le monde";
+}
